f4b92d2dd7e779dffff1b85b5ed28509dbea489d
aart0328 <aart0328@localhost>
2014-11-11 10:27:01 -0600

    commit

--- bf-ws/src/main/java/com/optative/bf/ws/CustomObjectMapper.java
+++ bf-ws/src/main/java/com/optative/bf/ws/CustomObjectMapper.java
@@ -2,7 +2,6 @@ package com.optative.bf.ws;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.ser.CustomSerializerFactory;
-import org.joda.time.DateTime;
 
 public class CustomObjectMapper extends ObjectMapper {
 
