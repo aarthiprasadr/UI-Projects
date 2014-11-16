58b474f699afea6e901cb2bcdf774ce938ac3cb7
aart0328 <aart0328@localhost>
2014-11-16 14:50:31 -0600

    added device detect conttoller for mobile support

---
+++ bf-ws/src/main/java/com/optative/bf/ws/DeviceDetectionController.java
@@ -0,0 +1,42 @@
+package com.optative.bf.ws;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.mobile.device.Device;
+import org.springframework.mobile.device.DeviceUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DeviceDetectionController {
+
+	@RequestMapping("/detect-device")
+	public @ResponseBody String detectDevice(Device device) {
+		String deviceType = "unknown";
+		if (device.isNormal()) {
+			deviceType = "normal";
+		} else if (device.isMobile()) {
+			deviceType = "mobile";
+		} else if (device.isTablet()) {
+			deviceType = "tablet";
+		}
+		return "Hello " + deviceType + " browser!";
+	}
+
+	@RequestMapping("/detect-device1")
+	public @ResponseBody String preHandle(HttpServletRequest request,
+			HttpServletResponse arg1, Object arg2) throws Exception {
+		Device device = DeviceUtils.getCurrentDevice(request);
+		String deviceType = "unknown";
+		if (device.isNormal()) {
+			deviceType = "normal";
+		} else if (device.isMobile()) {
+			deviceType = "mobile";
+		} else if (device.isTablet()) {
+			deviceType = "tablet";
+		}
+		return deviceType;
+	}
+}
