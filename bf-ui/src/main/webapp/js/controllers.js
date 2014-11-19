a0decee367fc37c1f680aff3e0fb57a225849ef6
anto1314 <anto1314@L43XJNX1.RACKSPACE.CORP>
2014-11-18 22:19:33 -0600

    cross domain

--- bf-ui/src/main/webapp/js/controllers.js
+++ bf-ui/src/main/webapp/js/controllers.js
@@ -1,298 +1,330 @@
-angular.module('bfApp.controllers', []).
+angular
+		.module('bfApp.controllers', [])
+		.controller(
+				'AllDeals',
+				function($scope, $rootScope, bfService) {
+					$scope.nameFilter = null;
+					$scope.dealList = [];
 
-controller(
-		'AllDeals',
-		function($scope, bfService) {
-			$scope.nameFilter = null;
-			$scope.dealList = [];
+					$scope.currentPage = 0;
+					$scope.pageSize = 50;
+					$scope.storeFilter = null;
+					$scope.storeSearchFilter = function(store) {
+						var re = new RegExp($scope.storeFilter, 'i');
+						return !$scope.storeFilter || re.test(store);
+					};
 
-			$scope.currentPage = 0;
-			$scope.pageSize = 50;
-			$scope.storeFilter = null;
-			
-			$scope.storeSearchFilter = function(store) {
-				var re = new RegExp($scope.storeFilter, 'i');
-				return !$scope.storeFilter || re.test(store);
-			};
-			
-			$scope.categoryFilter = null;
-			
-			$scope.categorySearchFilter = function(category) {
-				var re = new RegExp($scope.categoryFilter, 'i');
-				return !$scope.categoryFilter || re.test(category);
-			};
+					$scope.categoryFilter = null;
 
-			$scope.searchFilter = function(deal) {
-				var re = new RegExp($scope.nameFilter, 'i');
-				var result = (!$scope.nameFilter || re.test(deal.item)
-						|| re.test(deal.store) || re.test(deal.category)
-						|| re.test(deal.sub_category)
-						|| re.test(deal.early_bird) || re.test(deal.rebate)
-						|| re.test(deal.price));
-				
-				return result;
-			};
+					$scope.categorySearchFilter = function(category) {
+						var re = new RegExp($scope.categoryFilter, 'i');
+						return !$scope.categoryFilter || re.test(category);
+					};
 
-			bfService.getDeals().success(function(response) {
-				$scope.dealList = response.deals;
-			});
+					$scope.searchFilter = function(deal) {
+						var re = new RegExp($scope.nameFilter, 'i');
+						var result = (!$scope.nameFilter || re.test(deal.item)
+								|| re.test(deal.store)
+								|| re.test(deal.category)
+								|| re.test(deal.sub_category)
+								|| re.test(deal.early_bird)
+								|| re.test(deal.rebate) || re.test(deal.price));
 
-			$scope.numberOfPages = function() {
-				return Math.ceil($scope.dealList.length / $scope.pageSize);
-			};
+						return result;
+					};
 
-			$scope.storeList = [];
-
-			bfService.getStores().success(function(response) {
-				$scope.storeList = response.storeNames;
-			});
-
-			$scope.categoryList = [];
-
-			bfService.getCategories().success(function(response) {
-				$scope.categoryList = response.categoryNames;
-			});
-			
-			$scope.mobile = false;
-
-			bfService.isMobile().success(function(response) {
-				var isMobileDevice = response.replace("\"", "");
-				isMobileDevice = isMobileDevice.replace("\"", "");
-				if(isMobileDevice === "mobile") {
-					$scope.mobile = true;
-				} else {
-					$scope.mobile = false;
-				}
-			});
-		}).
-
-filter('startFrom', function() {
-	return function(input, start) {
-		start = +start; // parse to int
-		return input.slice(start);
-	}
-}).
-
-controller(
-		'StoreDeals',
-		function($scope, $route, $location, $routeParams, bfService) {
-			var category = $routeParams.category;
-			var store = $routeParams.store;
-
-			$scope.selectedStore = store;
-			$scope.selectedCategory = category;
-			$scope.nameFilter = null;
-			$scope.storeFilter = null;
-			$scope.dealList = [];
-			$scope.categoryList = [];
-			
-			$scope.storeSearchFilter = function(store) {
-				var re = new RegExp($scope.storeFilter, 'i');
-				return !$scope.storeFilter || re.test(store);
-			};
-			
-			$scope.categoryFilter = null;
-			
-			$scope.categorySearchFilter = function(category) {
-				var re = new RegExp($scope.categoryFilter, 'i');
-				return !$scope.categoryFilter || re.test(category);
-			};
-
-			$scope.searchFilter = function(deal) {
-				var re = new RegExp($scope.nameFilter, 'i');
-				var result = (!$scope.nameFilter || re.test(deal.item)
-						|| re.test(deal.store) || re.test(deal.category)
-						|| re.test(deal.sub_category)
-						|| re.test(deal.early_bird) || re.test(deal.rebate)
-						|| re.test(deal.price));
-				
-				return result;
-			};
-
-			$scope.currentPage = 0;
-			$scope.pageSize = 50;
-			$scope.numberOfPages = function() {
-				return Math.ceil($scope.dealList.length / $scope.pageSize);
-			};
-
-			bfService.getDealsByStore(store).success(function(response) {
-				$scope.dealList = response.deals;
-			});
-
-			bfService.getCategoriesByStore(store).success(function(response) {
-				if (!angular.isUndefined(store) || store != null) {
-					$scope.categoryList = response.storeConfig.categoryConfigs;
-				} else {
-					$scope.categoryList = response.categoryNames;
-				}
-			});
-			
-			bfService.getStoresByCategory(category).success(function(response) {
-				if (!angular.isUndefined(category) || category != null) {
-					$scope.storeList = response.storeList.storeNames;
-				} else {
-					$scope.storeList = response.storeNames;
-				}
-			});
-			
-			$scope.mobile = false;
-
-			bfService.isMobile().success(function(response) {
-				var isMobileDevice = response.replace("\"", "");
-				isMobileDevice = isMobileDevice.replace("\"", "");
-				if(isMobileDevice === "mobile") {
-					$scope.mobile = true;
-				} else {
-					$scope.mobile = false;
-				}
-			});
-		}).
-
-controller(
-		'CategoryDeals',
-		function($scope, $route, $location, $routeParams, bfService) {
-			var category = $routeParams.category;
-			var store = $routeParams.store;
-
-			$scope.nameFilter = null;
-			$scope.selectedStore = store;
-			$scope.selectedCategory = category;
-			$scope.dealList = [];
-			$scope.storeList = [];
-			$scope.storeFilter = null;
-			
-			$scope.storeSearchFilter = function(store) {
-				var re = new RegExp($scope.storeFilter, 'i');
-				return !$scope.storeFilter || re.test(store);
-			};
-			
-			$scope.categoryFilter = null;
-			
-			$scope.categorySearchFilter = function(category) {
-				var re = new RegExp($scope.categoryFilter, 'i');
-				return !$scope.categoryFilter || re.test(category);
-			};
-
-			$scope.searchFilter = function(deal) {
-				var re = new RegExp($scope.nameFilter, 'i');
-				var result = (!$scope.nameFilter || re.test(deal.item)
-						|| re.test(deal.store) || re.test(deal.category)
-						|| re.test(deal.sub_category)
-						|| re.test(deal.early_bird) || re.test(deal.rebate)
-						|| re.test(deal.price));				
-				return result;
-			};
-
-			bfService.getDealsByCategory(category, store).success(
-					function(response) {
+					bfService.getDeals().success(function(response) {
 						$scope.dealList = response.deals;
-			});
+					});
 
-			bfService.getStoresByCategory(category).success(function(response) {
-				if (!angular.isUndefined(category) || category != null) {
-					$scope.storeList = response.storeList.storeNames;
-				} else {
-					$scope.storeList = response.storeNames;
-				}
-			});
-			
-			bfService.getCategoriesByStore(store).success(function(response) {
-				if (!angular.isUndefined(store) || store != null) {
-					$scope.categoryList = response.storeConfig.categoryConfigs;
-				} else {
-					$scope.categoryList = response.categoryNames;
-				}				
-			});
+					$scope.numberOfPages = function() {
+						return Math.ceil($scope.dealList.length
+								/ $scope.pageSize);
+					};
 
-			$scope.currentPage = 0;
-			$scope.pageSize = 50;
-			$scope.numberOfPages = function() {
-				return Math.ceil($scope.dealList.length / $scope.pageSize);
-			};
-			
-			$scope.mobile = false;
+					$scope.storeList = [];
 
-			bfService.isMobile().success(function(response) {
-				var isMobileDevice = response.replace("\"", "");
-				isMobileDevice = isMobileDevice.replace("\"", "");
-				if(isMobileDevice === "mobile") {
-					$scope.mobile = true;
-				} else {
+					bfService.getStores().success(function(response) {
+						$scope.storeList = response.storeNames;
+					});
+
+					$scope.categoryList = [];
+
+					bfService.getCategories().success(function(response) {
+						$scope.categoryList = response.categoryNames;
+					});
+
 					$scope.mobile = false;
-				}
-			});
+
+					bfService.isMobile().success(function(response) {
+						var isMobileDevice = response.replace("\"", "");
+						isMobileDevice = isMobileDevice.replace("\"", "");
+						if (isMobileDevice === "mobile") {
+							$scope.mobile = true;
+						} else {
+							$scope.mobile = false;
+						}
+					});
+				}).
+
+		filter('startFrom', function() {
+			return function(input, start) {
+				start = +start; // parse to int
+				return input.slice(start);
+			}
 		}).
 
-controller(
-		'StoreCategoryDeals',
-		function($scope, $route, $location, $routeParams, bfService) {
-			var category = $routeParams.category;
-			var store = $routeParams.store;
+		controller(
+				'StoreDeals',
+				function($scope, $route, $location, $routeParams, bfService) {
+					var category = $routeParams.category;
+					var store = $routeParams.store;
 
-			$scope.nameFilter = null;
-			$scope.selectedStore = store;
-			$scope.selectedCategory = category;
-			$scope.dealList = [];
-			$scope.storeFilter = null;
-			
-			$scope.storeSearchFilter = function(store) {
-				var re = new RegExp($scope.storeFilter, 'i');
-				return !$scope.storeFilter || re.test(store);
-			};
-			
-			$scope.categoryFilter = null;
-			
-			$scope.categorySearchFilter = function(category) {
-				var re = new RegExp($scope.categoryFilter, 'i');
-				return !$scope.categoryFilter || re.test(category);
-			};
+					$scope.selectedStore = store;
+					$scope.selectedCategory = category;
+					$scope.nameFilter = null;
+					$scope.storeFilter = null;
+					$scope.dealList = [];
+					$scope.categoryList = [];
 
-			$scope.searchFilter = function(deal) {
-				var re = new RegExp($scope.nameFilter, 'i');
-				var result = (!$scope.nameFilter || re.test(deal.item)
-						|| re.test(deal.store) || re.test(deal.category)
-						|| re.test(deal.sub_category)
-						|| re.test(deal.early_bird) || re.test(deal.rebate)
-						|| re.test(deal.price));
-				return result;
-			};
+					$scope.storeSearchFilter = function(store) {
+						var re = new RegExp($scope.storeFilter, 'i');
+						return !$scope.storeFilter || re.test(store);
+					};
 
-			bfService.getDealsByStoreCategory(store, category).success(
-					function(response) {
-						$scope.dealList = response.dealList.deals;
-			});
-			
-			bfService.getStoresByCategory(category).success(function(response) {
-				if (!angular.isUndefined(category) || category != null) {
-					$scope.storeList = response.storeList.storeNames;
-				} else {
-					$scope.storeList = response.storeNames;
-				}
-			});
-			
-			bfService.getCategoriesByStore(store).success(function(response) {
-				if (!angular.isUndefined(store) || store != null) {
-					$scope.categoryList = response.storeConfig.categoryConfigs;
-				} else {
-					$scope.categoryList = response.categoryNames;
-				}				
-			});
+					$scope.categoryFilter = null;
 
-			$scope.currentPage = 0;
-			$scope.pageSize = 50;
-			$scope.numberOfPages = function() {
-				return Math.ceil($scope.dealList.length / $scope.pageSize);
-			};
-			
-			$scope.mobile = false;
+					$scope.categorySearchFilter = function(category) {
+						var re = new RegExp($scope.categoryFilter, 'i');
+						return !$scope.categoryFilter || re.test(category);
+					};
 
-			bfService.isMobile().success(function(response) {
-				var isMobileDevice = response.replace("\"", "");
-				isMobileDevice = isMobileDevice.replace("\"", "");
-				if(isMobileDevice === "mobile") {
-					$scope.mobile = true;
-				} else {
+					$scope.searchFilter = function(deal) {
+						var re = new RegExp($scope.nameFilter, 'i');
+						var result = (!$scope.nameFilter || re.test(deal.item)
+								|| re.test(deal.store)
+								|| re.test(deal.category)
+								|| re.test(deal.sub_category)
+								|| re.test(deal.early_bird)
+								|| re.test(deal.rebate) || re.test(deal.price));
+
+						return result;
+					};
+
+					$scope.currentPage = 0;
+					$scope.pageSize = 50;
+					$scope.numberOfPages = function() {
+						return Math.ceil($scope.dealList.length
+								/ $scope.pageSize);
+					};
+
+					bfService.getDealsByStore(store).success(
+							function(response) {
+								$scope.dealList = response.deals;
+							});
+
+					bfService
+							.getCategoriesByStore(store)
+							.success(
+									function(response) {
+										if (!angular.isUndefined(store)
+												|| store != null) {
+											$scope.categoryList = response.storeConfig.categoryConfigs;
+										} else {
+											$scope.categoryList = response.categoryNames;
+										}
+									});
+
+					bfService
+							.getStoresByCategory(category)
+							.success(
+									function(response) {
+										if (!angular.isUndefined(category)
+												|| category != null) {
+											$scope.storeList = response.storeList.storeNames;
+										} else {
+											$scope.storeList = response.storeNames;
+										}
+									});
+
 					$scope.mobile = false;
-				}
-			});
-		});
+
+					bfService.isMobile().success(function(response) {
+						var isMobileDevice = response.replace("\"", "");
+						isMobileDevice = isMobileDevice.replace("\"", "");
+						if (isMobileDevice === "mobile") {
+							$scope.mobile = true;
+						} else {
+							$scope.mobile = false;
+						}
+					});
+				}).
+
+		controller(
+				'CategoryDeals',
+				function($scope, $route, $location, $routeParams, bfService) {
+					var category = $routeParams.category;
+					var store = $routeParams.store;
+
+					$scope.nameFilter = null;
+					$scope.selectedStore = store;
+					$scope.selectedCategory = category;
+					$scope.dealList = [];
+					$scope.storeList = [];
+					$scope.storeFilter = null;
+
+					$scope.storeSearchFilter = function(store) {
+						var re = new RegExp($scope.storeFilter, 'i');
+						return !$scope.storeFilter || re.test(store);
+					};
+
+					$scope.categoryFilter = null;
+
+					$scope.categorySearchFilter = function(category) {
+						var re = new RegExp($scope.categoryFilter, 'i');
+						return !$scope.categoryFilter || re.test(category);
+					};
+
+					$scope.searchFilter = function(deal) {
+						var re = new RegExp($scope.nameFilter, 'i');
+						var result = (!$scope.nameFilter || re.test(deal.item)
+								|| re.test(deal.store)
+								|| re.test(deal.category)
+								|| re.test(deal.sub_category)
+								|| re.test(deal.early_bird)
+								|| re.test(deal.rebate) || re.test(deal.price));
+						return result;
+					};
+
+					bfService.getDealsByCategory(category, store).success(
+							function(response) {
+								$scope.dealList = response.deals;
+							});
+
+					bfService
+							.getStoresByCategory(category)
+							.success(
+									function(response) {
+										if (!angular.isUndefined(category)
+												|| category != null) {
+											$scope.storeList = response.storeList.storeNames;
+										} else {
+											$scope.storeList = response.storeNames;
+										}
+									});
+
+					bfService
+							.getCategoriesByStore(store)
+							.success(
+									function(response) {
+										if (!angular.isUndefined(store)
+												|| store != null) {
+											$scope.categoryList = response.storeConfig.categoryConfigs;
+										} else {
+											$scope.categoryList = response.categoryNames;
+										}
+									});
+
+					$scope.currentPage = 0;
+					$scope.pageSize = 50;
+					$scope.numberOfPages = function() {
+						return Math.ceil($scope.dealList.length
+								/ $scope.pageSize);
+					};
+
+					$scope.mobile = false;
+
+					bfService.isMobile().success(function(response) {
+						var isMobileDevice = response.replace("\"", "");
+						isMobileDevice = isMobileDevice.replace("\"", "");
+						if (isMobileDevice === "mobile") {
+							$scope.mobile = true;
+						} else {
+							$scope.mobile = false;
+						}
+					});
+				}).
+
+		controller(
+				'StoreCategoryDeals',
+				function($scope, $route, $location, $routeParams, bfService) {
+					var category = $routeParams.category;
+					var store = $routeParams.store;
+
+					$scope.nameFilter = null;
+					$scope.selectedStore = store;
+					$scope.selectedCategory = category;
+					$scope.dealList = [];
+					$scope.storeFilter = null;
+
+					$scope.storeSearchFilter = function(store) {
+						var re = new RegExp($scope.storeFilter, 'i');
+						return !$scope.storeFilter || re.test(store);
+					};
+
+					$scope.categoryFilter = null;
+
+					$scope.categorySearchFilter = function(category) {
+						var re = new RegExp($scope.categoryFilter, 'i');
+						return !$scope.categoryFilter || re.test(category);
+					};
+
+					$scope.searchFilter = function(deal) {
+						var re = new RegExp($scope.nameFilter, 'i');
+						var result = (!$scope.nameFilter || re.test(deal.item)
+								|| re.test(deal.store)
+								|| re.test(deal.category)
+								|| re.test(deal.sub_category)
+								|| re.test(deal.early_bird)
+								|| re.test(deal.rebate) || re.test(deal.price));
+						return result;
+					};
+
+					bfService.getDealsByStoreCategory(store, category).success(
+							function(response) {
+								$scope.dealList = response.dealList.deals;
+							});
+
+					bfService
+							.getStoresByCategory(category)
+							.success(
+									function(response) {
+										if (!angular.isUndefined(category)
+												|| category != null) {
+											$scope.storeList = response.storeList.storeNames;
+										} else {
+											$scope.storeList = response.storeNames;
+										}
+									});
+
+					bfService
+							.getCategoriesByStore(store)
+							.success(
+									function(response) {
+										if (!angular.isUndefined(store)
+												|| store != null) {
+											$scope.categoryList = response.storeConfig.categoryConfigs;
+										} else {
+											$scope.categoryList = response.categoryNames;
+										}
+									});
+
+					$scope.currentPage = 0;
+					$scope.pageSize = 50;
+					$scope.numberOfPages = function() {
+						return Math.ceil($scope.dealList.length
+								/ $scope.pageSize);
+					};
+
+					$scope.mobile = false;
+
+					bfService.isMobile().success(function(response) {
+						var isMobileDevice = response.replace("\"", "");
+						isMobileDevice = isMobileDevice.replace("\"", "");
+						if (isMobileDevice === "mobile") {
+							$scope.mobile = true;
+						} else {
+							$scope.mobile = false;
+						}
+					});
+				});
--- bf-ui/src/main/webapp/js/services.js
+++ bf-ui/src/main/webapp/js/services.js
@@ -1,146 +1,158 @@
 /**
  * Notification Service Module
  */
-angular.module('bfApp.services', []).factory(
-		'bfService',
-		function($http) {
+angular
+		.module('bfApp.services', [])
+		.factory(
+				'bfService',
+				function($http) {
 
-			var bfAPI = {};
-			bfAPI.getDealsByStore = function($store) {
-				return $http({
-					method : 'GET',
-					headers : {
-						'accept' : 'application/json'
-					},
-					url : 'http://www.fridaywallet.com/bf-ws/deals/store/' + $store
+					var bfAPI = {};
+					bfAPI.getDealsByStore = function($store) {
+						return $http({
+							method : 'JSONP',
+							headers : {
+								'accept' : 'application/json'
+							},
+							url : 'http://www.fridaywallet.com/bf-ws/deals/store/'
+									+ $store + '.json?callback=JSON_CALLBACK'
+						});
+					}
+
+					bfAPI.getDealsByCategory = function($category, $store) {
+						if (angular.isUndefined($store) || $store == null) {
+							return $http({
+								method : 'JSONP',
+								headers : {
+									'accept' : 'application/json'
+								},
+								url : 'http://www.fridaywallet.com/bf-ws/deals/category/'
+										+ $category
+										+ '.json?callback=JSON_CALLBACK'
+							});
+						} else {
+							return $http({
+								method : 'JSONP',
+								headers : {
+									'accept' : 'application/json'
+								},
+								url : 'http://www.fridaywallet.com/bf-ws/deals/store/'
+										+ $store
+										+ '/category/'
+										+ $category
+										+ '.json?callback=JSON_CALLBACK'
+
+							});
+						}
+					}
+
+					bfAPI.getDeals = function() {
+						return $http({
+							method : 'JSONP',
+							headers : {
+								'accept' : 'application/json'
+							},
+							url : 'http://www.fridaywallet.com/bf-ws/deals.json?callback=JSON_CALLBACK'
+						});
+					}
+
+					bfAPI.getStores = function() {
+						return $http({
+							method : 'JSONP',
+							headers : {
+								'accept' : 'application/json'
+							},
+							url : 'http://www.fridaywallet.com/bf-ws/stores.json?callback=JSON_CALLBACK'
+						});
+					}
+
+					bfAPI.getCategories = function() {
+						return $http({
+							method : 'JSONP',
+							headers : {
+								'accept' : 'application/json'
+							},
+							url : 'http://www.fridaywallet.com/bf-ws/category.json?callback=JSON_CALLBACK'
+						});
+					}
+
+					bfAPI.getSubCategories = function() {
+						return $http({
+							method : 'JSONP',
+							headers : {
+								'accept' : 'application/json'
+							},
+							url : 'http://www.fridaywallet.com/bf-ws/sub-category.json?callback=JSON_CALLBACK'
+						});
+					}
+
+					bfAPI.getCategoriesByStore = function($store) {
+						if (!angular.isUndefined($store) || $store != null) {
+							return $http({
+								method : 'JSONP',
+								headers : {
+									'accept' : 'application/json'
+								},
+								url : 'http://www.fridaywallet.com/bf-ws/store-config/store/'
+										+ $store
+										+ '.json?callback=JSON_CALLBACK'
+							});
+						} else {
+							return $http({
+								method : 'JSONP',
+								headers : {
+									'accept' : 'application/json'
+								},
+								url : 'http://www.fridaywallet.com/bf-ws/category.json?callback=JSON_CALLBACK'
+							});
+						}
+					}
+
+					bfAPI.getDealsByStoreCategory = function($store, $category) {
+						return $http({
+							method : 'JSONP',
+							headers : {
+								'accept' : 'application/json'
+							},
+							url : 'http://www.fridaywallet.com/bf-ws/deals/store/'
+									+ $store + '/category/' + $category
+									+ '.json?callback=JSON_CALLBACK'
+						});
+					}
+
+					bfAPI.isMobile = function() {
+						return $http({
+							method : 'JSONP',
+							headers : {
+								'accept' : 'application/json'
+							},
+							url : 'http://www.fridaywallet.com/bf-ws/detect-device.json?callback=JSON_CALLBACK'
+						});
+					}
+
+					bfAPI.getStoresByCategory = function($category) {
+						if (!angular.isUndefined($category)
+								|| $category != null) {
+							return $http({
+								method : 'JSONP',
+								headers : {
+									'accept' : 'application/json'
+								},
+								url : 'http://www.fridaywallet.com/bf-ws/stores/category/'
+										+ $category
+										+ '.json?callback=JSON_CALLBACK'
+							});
+						} else {
+							return $http({
+								method : 'JSONP',
+								headers : {
+									'accept' : 'application/json'
+								},
+								url : 'http://www.fridaywallet.com/bf-ws/stores.json?callback=JSON_CALLBACK'
+							});
+						}
+					}
+					
+
+					return bfAPI;
 				});
-			}
-
-			bfAPI.getDealsByCategory = function($category, $store) {
-				if (angular.isUndefined($store) || $store == null) {
-					return $http({
-						method : 'GET',
-						headers : {
-							'accept' : 'application/json'
-						},
-						url : 'http://www.fridaywallet.com/bf-ws/deals/category/'
-								+ $category
-					});
-				} else {
-					return $http({
-						method : 'GET',
-						headers : {
-							'accept' : 'application/json'
-						},
-						url : 'http://www.fridaywallet.com/bf-ws/deals/store/'
-								+ $store + '/category/' + $category
-
-					});
-				}
-			}
-
-			bfAPI.getDeals = function() {
-				return $http({
-					method : 'GET',
-					headers : {
-						'accept' : 'application/json'
-					},
-					url : 'http://www.fridaywallet.com/bf-ws/deals'
-				});
-			}
-
-			bfAPI.getStores = function() {
-				return $http({
-					method : 'GET',
-					headers : {
-						'accept' : 'application/json'
-					},
-					url : 'http://www.fridaywallet.com/bf-ws/stores'
-				});
-			}
-
-			bfAPI.getCategories = function() {
-				return $http({
-					method : 'GET',
-					headers : {
-						'accept' : 'application/json'
-					},
-					url : 'http://www.fridaywallet.com/bf-ws/category'
-				});
-			}
-
-			bfAPI.getSubCategories = function() {
-				return $http({
-					method : 'GET',
-					headers : {
-						'accept' : 'application/json'
-					},
-					url : 'http://www.fridaywallet.com/bf-ws/sub-category'
-				});
-			}
-
-			bfAPI.getCategoriesByStore = function($store) {
-				if (!angular.isUndefined($store) || $store != null) {
-					return $http({
-						method : 'GET',
-						headers : {
-							'accept' : 'application/json'
-						},
-						url : 'http://www.fridaywallet.com/bf-ws/store-config/store/'
-								+ $store
-					});
-				} else {
-					return $http({
-						method : 'GET',
-						headers : {
-							'accept' : 'application/json'
-						},
-						url : 'http://www.fridaywallet.com/bf-ws/category'
-					});
-				}
-			}
-
-			bfAPI.getDealsByStoreCategory = function($store, $category) {
-				return $http({
-					method : 'GET',
-					headers : {
-						'accept' : 'application/json'
-					},
-					url : 'http://www.fridaywallet.com/bf-ws/deals/store/' + $store
-							+ '/category/' + $category
-				});
-			}
-			
-			bfAPI.isMobile = function() {
-				return $http({
-					method : 'GET',
-					headers : {
-						'accept' : 'application/json'
-					},
-					url : 'http://www.fridaywallet.com/bf-ws/detect-device'
-				});
-			}
-
-			bfAPI.getStoresByCategory = function($category) {
-				if (!angular.isUndefined($category) || $category != null) {
-					return $http({
-						method : 'GET',
-						headers : {
-							'accept' : 'application/json'
-						},
-						url : 'http://www.fridaywallet.com/bf-ws/stores/category/'
-								+ $category
-					});
-				} else {
-					return $http({
-						method : 'GET',
-						headers : {
-							'accept' : 'application/json'
-						},
-						url : 'http://www.fridaywallet.com/bf-ws/stores'
-					});
-				}
-			}
-
-			return bfAPI;
-		});
